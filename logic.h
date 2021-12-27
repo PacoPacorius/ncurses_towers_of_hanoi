559dfc00544ec3f8eba9e3d418e3c794afc1c433
PacoPacorius <74704581+PacoPacorius@users.noreply.github.com>
2021-12-27 15:21:29 +0200

    working stack v2 - minor readability improvement

--- logic.h
+++ logic.h
@@ -11,4 +11,8 @@ void logic(int input);
 
 void draw_poles(WINDOW* win, const int blocks[5]);
 
+void move_block(struct Stack* destinatio, struct Stack* source);
+
+void update_blocks(struct Stack* stack_array[3], WINDOW* win);
+
 #endif
